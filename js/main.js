5c4b5bff0e8afc5fdf7f0781feecb35a13c83dcf
Maxwell Calkin <101308415+MaxwellCalkin@users.noreply.github.com>
2022-05-31 14:53:21 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let turn = 1
-let playerID = ''
-let playerOPS = ''
+let playerID = 0
+let playerOPS = 0
 let scoreOne = 0
 let scoreTwo = 0
 let playerScore = 0
@@ -63,8 +63,8 @@ async function addPlayerAndScoreOne(){
     .catch(err => {
         console.log(`error ${err}`)
         document.querySelector('.error').classList.remove('hide')
-        // turn -= 1
-        // multipleMessUps = true
+        turn -= 1
+        multipleMessUps = true
         playerID = 0
         console.log(turn)
     });
@@ -80,9 +80,9 @@ async function addPlayerAndScoreOne(){
     .catch(err => {
         console.log(`error ${err}`)
         document.querySelector('.error').classList.remove('hide')
-        // if(!multipleMessUps){ 
-        //   turn -= 1
-        // }
+        if(!multipleMessUps){ 
+          turn -= 1
+        }
         playerOPS = 0
         console.log(turn)
     });
@@ -111,43 +111,63 @@ async function addPlayerAndScoreOne(){
 //----------------------
 async function addPlayerAndScoreTwo(){
   const name = document.querySelector('input').value.toLowerCase()
+
+  //Get Player ID From Name In Input
   await fetch(`https://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&name_part='${name}'`)
     .then(res => res.json()) // parse response as JSON
-    .then(data => {
+    .then(async (data) => {
       console.log(data.search_player_all.queryResults.row.player_id)
       playerID = data.search_player_all.queryResults.row.player_id
+
+      //If There Are Multiple Players Of The Same name
       if(playerID === undefined){
-        playerID = data.search_player_all.queryResults.row[1].player_id
+        let highestOPS = 0
+        let row = data.search_player_all.queryResults.row
+        for(let i = 0; i < data.search_player_all.queryResults.row.length; i++){
+          await fetch(`https://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id=${String(row[i].player_id)}`)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            if(data.sport_career_hitting.queryResults.row.ops > highestOPS){
+              highestOPS = data.sport_career_hitting.queryResults.row.ops
+              playerID = row[i].player_id
+            }
+          })
+          .catch(err => {
+            console.log(`error ${err}, there was a problem getting a multiple player name's ops`)
+          })
+        }
+        playerOPS = highestOPS
       }
     })
     .catch(err => {
         console.log(`error ${err}`)
         document.querySelector('.error').classList.remove('hide')
-        // if(!multipleMessUps){ 
-        //   turn -= 1
-        //   multipleMessUps = true
-        // }
+        turn -= 1
+        multipleMessUps = true
         playerID = 0
         return 1
     });
 
-  await fetch(`https://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id=${String(playerID)}`)
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data.sport_career_hitting.queryResults.row.ops)
-    playerOPS = data.sport_career_hitting.queryResults.row.ops
-  })
-  .catch(err => {
-      console.log(`error ${err}`)
-      document.querySelector('.error').classList.remove('hide')
-      // if(!multipleMessUps){ 
-      //   turn -= 1
-      //   multipleMessUps = true
-      // }
-      playerOPS = 0
-      console.log(turn)
-  });
-
+  //If OPS Not Already Found, Find OPS
+  if(playerOPS === 0){  
+    await fetch(`https://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id=${String(playerID)}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.sport_career_hitting.queryResults.row.ops)
+      playerOPS = data.sport_career_hitting.queryResults.row.ops
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+        document.querySelector('.error').classList.remove('hide')
+        if(!multipleMessUps){ 
+          turn -= 1
+          multipleMessUps = true
+        }
+        playerOPS = 0
+        console.log(turn)
+    });
+  }
   scoreTwo = Number(document.querySelector('.teamTwoScore').innerText)
   playerScore = (+playerOPS*1000) + Math.round((Math.random() * 50 - 25))
   
@@ -167,15 +187,17 @@ async function addPlayerAndScoreTwo(){
 
 
 function checkWin(){
-  if(Number(document.querySelector('.teamOneScore').innerText) > Number(document.querySelector('.teamTwoScore').innerText)){
-    document.querySelector('.playerOne').innerText = 'PLAYER ONE WINS!!!!!'
-  }else if(Number(document.querySelector('.teamOneScore').innerText) < Number(document.querySelector('.teamTwoScore').innerText)){
-    document.querySelector('.playerOne').innerText = 'PLAYER TWO WINS!!!!!'
-  }else{
-    document.querySelector('.playerOne').innerText = 'IT\'S A TIE?!?!?!?!'
+  if(turn > 18){
+    if(Number(document.querySelector('.teamOneScore').innerText) > Number(document.querySelector('.teamTwoScore').innerText)){
+      document.querySelector('.playerOne').innerText = 'PLAYER ONE WINS!!!!!'
+    }else if(Number(document.querySelector('.teamOneScore').innerText) < Number(document.querySelector('.teamTwoScore').innerText)){
+      document.querySelector('.playerOne').innerText = 'PLAYER TWO WINS!!!!!'
+    }else{
+      document.querySelector('.playerOne').innerText = 'IT\'S A TIE?!?!?!?!'
+    }
+    document.querySelector('.error').innerText = 'Refresh Page To Play Again :)'
+    document.querySelector('.error').classList.remove('hide')
   }
-  document.querySelector('.error').innerText = 'Refresh Page To Play Again :)'
-  document.querySelector('.error').classList.remove('hide')
 }
 
 
@@ -185,19 +207,20 @@ async function playBall(count){
   if(turn > 18){
     document.querySelectorAll('.error').innerText = 'Refresh Page To Play Again :)'
     checkWin()
-    gameContinues = false
   }
   else if(turn%2){
     await addPlayerAndScoreOne()
-    if(playerID !== undefined && playerOPS !== undefined){
+    if(playerID !== undefined){
       turn += 1
     }
+    checkWin()
     console.log(turn)
   }else{
     await addPlayerAndScoreTwo()
-    if(playerID !== undefined && playerOPS !== undefined){
+    if(playerID !== undefined){
       turn += 1
     }
+    checkWin()
     console.log(turn)
   }
 }
